b5a9c86122005dfe7dc96aa0a873bd551295eced
Olexiy <a1980dominov@gmail.com>
2022-09-08 11:48:13 +0300

    Clock

---
+++ secundomer.js
@@ -0,0 +1,31 @@
+
+let x=150;
+let  can= document.getElementById("area")
+let canvas=can.getContext('2d')
+canvas.lineWidth=1
+canvas.strokeStyle='green'
+canvas.beginPath()
+canvas.arc(150,70,60,0,Math.PI*2,false)
+canvas.stroke()
+canvas.strokeStyle='black'
+canvas.lineWidth=1
+canvas.beginPath()
+canvas.moveTo(x,15)
+canvas.lineTo(150,70)
+canvas.stroke()
+let sec=0
+let minute=document.getElementById('minute')
+minute.innerText='0'
+setInterval( function (){
+
+    can.style.transform='rotate('+sec+'deg)'
+    sec=sec+6
+    console.log(sec)
+    if(sec==360){
+        minute.innerText++
+        sec=0
+    }
+},1000)
+
+
+
